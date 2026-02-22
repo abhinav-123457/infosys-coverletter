5631228a4f2f388db6a1e59522958c35b04d6425
Abhinav Shakya <136550261+abhinav-123457@users.noreply.github.com>
2026-02-22 16:55:05 +0530

    Delete grammar_polisher.py

--- grammar_polisher.py
+++
@@ -1,8 +0,0 @@
-import language_tool_python
-
-tool = language_tool_python.LanguageTool('en-US')
-
-def polish_paragraph(paragraph: str) -> str:
-    matches = tool.check(paragraph)
-    corrected = language_tool_python.utils.correct(paragraph, matches)
-    return corrected
